075526c6c22f7a8cd971b1285926dac3b8e40f15
M1U5T0N3 (Marius Kopp) <mariuskopp517@gmail.com>
2021-10-07 02:28:47 +0200

    V211007
    Adds and Odds

--- MARS_SOM_SYSTEM/customize.sh
+++ MARS_SOM_SYSTEM/customize.sh
@@ -6,28 +6,7 @@ DYNLIB=true
 DEBUG=false
 
 REPLACE="
-/system/app/DaxUI
-/system/app/SomcXperiaServices
 /system/app/Stk
-/system/app/com.booking
-/system/app/com.epicgames.portal
-/system/app/com.facebook.appmanager
-/system/app/com.facebook.katana
-/system/app/com.gameloft.android.anmp.glofta9hm
-/system/app/com.netflix.mediaclient
-/system/app/com.sonymobile.support
-/system/app/daxService
-/system/priv-app/SemcAlbum-albumLive-release
-/system/priv-app/SemcMusic
-/system/priv-app/SemcPhotoEditor
-/system/priv-app/SemcPhotoEditor
-/system/priv-app/UpdateCenter-release
-/system/priv-app/com.facebook.services
-/system/priv-app/com.facebook.system
-/system/product/app/Music2
-/system/product/app/Stk
-/system/product/priv-app/SettingsIntelligence
-/system/product/priv-app/StorageManager
 "
 
 # PERMISSIONS
