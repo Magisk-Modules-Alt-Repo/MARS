4093a59c19c3dead75bc53c82b98e55b234e44e0
M1U5T0N3 <mariuskopp517@gmail.com>
2021-11-19 04:53:05 +0100

    V211119
    -Cleanup for MARK 3 Devices
    -Optimized Customize Shell Script
    -SONY Audio Enhancement Preperation

--- MARS_SOM_SYSTEM/customize.sh
+++ MARS_SOM_SYSTEM/customize.sh
@@ -5,39 +5,9 @@ MAXAPI=31
 DYNLIB=false
 DEBUG=false
 
-REPLACE="
-/system/app/DaxUI
-/system/app/SomcXperiaServices
-/system/app/Stk
-/system/app/com.booking
-/system/app/com.epicgames.portal
-/system/app/com.facebook.appmanager
-/system/app/com.facebook.katana
-/system/app/com.gameloft.android.anmp.glofta9hm
-/system/app/com.netflix.mediaclient
-/system/app/com.sonymobile.support
-/system/app/daxService
-/system/media/bootanimation.zip
-/system/priv-app/SemcAlbum-albumLive-release
-/system/priv-app/SemcMusic
-/system/priv-app/SemcPhotoEditor
-/system/priv-app/SemcPhotoEditor
-/system/priv-app/SomcUiccDetection
-/system/priv-app/UpdateCenter-release
-/system/priv-app/com.facebook.services
-/system/priv-app/com.facebook.system
-/system/product/app/Music2
-/system/product/app/Stk
-/system/product/priv-app/StorageManager
-"
-
-# PERMISSIONS
-
 set_permissions() {
   : 
 }
-
-# MMT
 
 SKIPUNZIP=1
 unzip -qjo "$ZIPFILE" 'common/functions.sh' -d $TMPDIR >&2
